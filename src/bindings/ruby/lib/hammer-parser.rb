971e4e4f70ff87da7609845f27a349a9ec5e00d4
Prashant Anantharaman <prashant.barca@gmail.com>
2016-09-11 10:05:18 -0700

    Change documentation and examples to reflect filename change

--- src/bindings/ruby/lib/hammer-parser.rb
+++ src/bindings/ruby/lib/hammer-parser.rb
@@ -2,11 +2,6 @@ require 'hammer/internal'
 require 'hammer/parser'
 require 'hammer/parser_builder'
 
-# TODO:
-# Probably need to rename this file to 'hammer-parser.rb', so
-# people can use "require 'hammer-parser'" in their code.
-
-
 # Leave this in for now to be able to play around with HParseResult in irb.
 x = nil
 parser = Hammer::Parser.build {
